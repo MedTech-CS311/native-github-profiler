df9a4bb72cc11705f0367862883b1a22957fd96b
Ikdemm <ikdem.benmbarek@aiesec.net>
2022-02-28 12:22:48 +0100

    used environment variables

--- redux/profile/profile.actions.js
+++ redux/profile/profile.actions.js
@@ -1,13 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+import { BASE_API_URL, GITHUB_TOKEN } from "@env";
+
 export const fetchUserData = createAsyncThunk(
   "profile/fetchUserData",
   async () => {
     try {
-      const response = await axios.get(`https://api.github.com/user`, {
+      const response = await axios.get(`${BASE_API_URL}/user`, {
         headers: {
-          Authorization: "token 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",
+          Authorization: `token ${GITHUB_TOKEN}`,
         },
       });
       return response.data;
--- redux/repos/repos.actions.js
+++ redux/repos/repos.actions.js
@@ -1,13 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+import { BASE_API_URL, GITHUB_TOKEN } from "@env";
+
 export const fetchUserRepos = createAsyncThunk(
   "repos/fetchUserRepos",
   async () => {
     try {
       const response = await axios.get(`https://api.github.com/user/repos`, {
         headers: {
-          Authorization: `token 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`,
+          Authorization: `token ${GITHUB_TOKEN}`,
         },
       });
       return response.data;
